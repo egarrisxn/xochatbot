cd057e2359ccf729db252145805086b90e83074a
egarrisxn <egarrisxn@gmail.com>
2025-03-08 02:35:14 -0500

    donzo!

--- app/(pages)/privacy/page.jsx
+++ app/(pages)/privacy/page.jsx
@@ -1,12 +1,15 @@
 export default function PrivacyPolicy() {
   return (
-    <div className='mx-auto max-w-4xl p-6'>
-      <h1 className='mb-4 text-3xl font-bold'>Privacy Policy</h1>
-      <p>
-        This site uses JSON Web Tokens and a Key-Value database for sessions. Data provided to this
-        site is exclusively used to support signing in and is not passed to any third party
-        services, other than via SMTP or OAuth for the purposes of authentication.
-      </p>
+    <div className="mx-auto grid place-items-center min-h-screen">
+      <div className="mx-auto max-w-2xl">
+        <h1 className="mb-4 text-3xl font-bold">Privacy Policy</h1>
+        <p>
+          This site uses JSON Web Tokens and a Key-Value database for sessions.
+          Data provided to this site is exclusively used to support signing in
+          and is not passed to any third party services, other than via SMTP or
+          OAuth for the purposes of authentication.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
